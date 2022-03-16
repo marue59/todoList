ab8f9656254cb6c562829f6a177ed00e2deda3c4
Marie <Marue59@hotmail.fr>
2022-03-16 18:53:25 +0100

    debug test fonctionnel home

--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -6,5 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class UserControllerTest extends WebTestCase
 {
-
+    private $client;
+  
+    public function setUp(): void
+    { 
+        
+        $this->client = self::createClient();
+    }
+    //Vérifier que le code status de la réponse HTTP est bien 200.
+    public function testHomepageIsUp()
+    {
+        
+        $client->request('GET', '/');
+        
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+    }
 }
